36cebe576d4ef754ac17f84173122f0e29901f61
Maxim Toma <maxim.toma@sky.uk>
2023-11-02 14:47:19 +0000

    Set up a databse in db.js

--- db.js
+++ db.js
@@ -1,9 +1,26 @@
-const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const path = require("path");
+const { Sequelize, Model } = require("sequelize");
 
 // TODO - create the new sequelize connection
 
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false,
+  define: {
+    timestamps: false,
+  },
+});
+
 module.exports = {
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize,
 };
+
+// npm init -y
+// npm i sequelize sqlite3
+// mkdir src db <-- folder, you make connection.js and the databse in there
+// FOR CREATED DATABSE
+// clears/resets the database each time you run it. Used for testing. Inside the async main function
+//await db.sync({ force: true });
+// console.log(JSON.stringify(variable_name, null, 2));

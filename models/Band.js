a7b08255f1661a3a1f78d4e769d8ddd03c9cac8c
Maxim Toma <maxim.toma@sky.uk>
2023-11-02 14:52:54 +0000

    Finished Band class/table

--- models/Band.js
+++ models/Band.js
@@ -4,10 +4,17 @@ const { db, Sequelize, DataTypes, Model } = require("../db");
 
 class Band extends Model {}
 
-Band.init({
-  name: DataTypes.STRING,
-  genre: DataTypes.STRING,
-});
+Band.init(
+  {
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING,
+  },
+  {
+    sequelize: db,
+    modelName: "Band",
+    freezeTableName: true,
+  }
+);
 
 module.exports = {
   Band,

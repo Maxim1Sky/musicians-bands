dfaf1a9fc76a2fa6e3cb4df73d564e556893d882
Maxim Toma <maxim.toma@sky.uk>
2023-11-02 14:54:45 +0000

    Created Musician class/table

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,19 @@
 const { db, Sequelize, DataTypes, Model } = require("../db");
 
 // TODO - define the Musician model
-let Musician;
+class Musician extends Model {}
+
+Musician.init(
+  {
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING,
+  },
+  {
+    sequelize: db,
+    tableName: "Musician",
+    freezeTableName: true,
+  }
+);
 
 module.exports = {
   Musician,
